1d4beeb3f6446fe2f9a65ec9c270cae65cd0d129
Anjali Silawat <silawatanjali0@gmail.com>
2026-05-31 21:12:05 +0530

    update cors and render url

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://task-manager-app-anjali.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Debug
@@ -24,7 +30,6 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server running on port ${process.env.PORT || 8000}`);
     });
